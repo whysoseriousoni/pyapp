5180fd6a83d173400e51c7edc2c98cac5050592a
whysoseriousoni <sweetmolu9@gmail.com>
2020-11-07 23:05:16 +0530

    2nd

--- pyapp/urls.py
+++ pyapp/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import hello.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello.views.index),
 ]
